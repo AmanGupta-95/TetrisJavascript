4dbccc18389f76014180155f659e5ca3df67ecff
Aman gupta <tysonzatn@gmail.com>
2021-04-09 20:34:51 +0530

    initialized the canvas

---
+++ tetris.js
@@ -0,0 +1,8 @@
+// initialized the canvas
+const canvas = document.querySelector('.my-canvas');
+// canvas dimension
+canvas.width = 260;
+canvas.height = 400;
+
+//canvas context
+const ctx = canvas.getContext('2d');
